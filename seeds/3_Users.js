6c52a095e839623efb475fc289ac66a7287bb622
Deshane McKenzie <122234024+Deshane-McKenzie@users.noreply.github.com>
2023-10-14 16:43:32 -0400

    added emails & passwords to seed files

--- seeds/3_Users.js
+++ seeds/3_Users.js
@@ -10,144 +10,144 @@ exports.seed = async function(knex) {
       user_id: 1,
       first_name: "Sarah",
       last_name: "Kim",
-      email: null,
-      password: null,
+      email: "sarah.kim@example.com",
+      password: "sarah.kim",
       phone_number: "416-534-2243"
     },
     {
       user_id: 2,
       first_name: "Robyn",
       last_name: "Jacobs",
-      email: null,
-      password: null,
+      email: "robyn.jacobs@example.com",
+      password: "robyn.jacobs",
       phone_number: "647-555-5595"
     },
     {
       user_id: 3,
       first_name: "Freddie",
       last_name: "Taylor",
-      email: null,
-      password: null,
+      email: "freddie.taylor@example.com",
+      password: "freddie.taylor",
       phone_number: "905-444-4484"
     },
     {
       user_id: 4,
       first_name: "Nicola",
       last_name: "Lucero",
-      email: null,
-      password: null,
+      email: "nicola.lucero@example.com",
+      password: "nicola.lucero",
       phone_number: "444-454-4489"
     },
     {
       user_id: 5,
       first_name: "Levi",
       last_name: "Walter",
-      email: null,
-      password: null,
+      email: "levi.walter@example.com",
+      password: "levi.walter",
       phone_number: "905-555-5555"
     },
     {
       user_id: 6,
       first_name: "Ines",
       last_name: "Lopez",
-      email: null,
-      password: null,
+      email: "ines.lopez@example.com",
+      password: "ines.lopez",
       phone_number: "416-444-4416"
     },
     {
       user_id: 7,
       first_name: "Macy",
       last_name: "Cabrera",
-      email: null,
-      password: null,
+      email: "macy.cabrera@example.com",
+      password: "macy.cabrera",
       phone_number: "289-999-9989"
     },
     {
       user_id: 8,
       first_name: "Felix",
       last_name: "Mendez",
-      email: null,
-      password: null,
+      email: "felix.mendez@example.com",
+      password: "felix.mendez",
       phone_number: "340-333-3334"
     },
     {
       user_id: 9,
       first_name: "Jerome",
       last_name: "Finley",
-      email: null,
-      password: null,
+      email: "jerome.finley@example.com",
+      password: "jerome.finley",
       phone_number: "223-888-8823"
     },
     {
       user_id: 10,
       first_name: "Tommy-Lee",
       last_name: "Harris",
-      email: null,
-      password: null,
+      email: "tommylee.harris@example.com",
+      password: "tommylee.harris",
       phone_number: "213-213-2213"
     },
     {
       user_id: 11,
       first_name: "Aadam",
       last_name: "King",
-      email: null,
-      password: null,
+      email: "aadam.king@example.com",
+      password: "aadam.king",
       phone_number: "546-554-5466"
     },
     {
       user_id: 12,
       first_name: "Kaya",
       last_name: "Terry",
-      email: null,
-      password: null,
+      email: "kaya.terry@example.com",
+      password: "kaya.terry",
       phone_number: "416-111-1111"
     },
     {
       user_id: 13,
       first_name: "Chad",
       last_name: "Rollins",
-      email: null,
-      password: null,
+      email: "chad.rollins@example.com",
+      password: "chad.rollins",
       phone_number: "289-222-2119"
     },
     {
       user_id: 14,
       first_name: "Tanya",
       last_name: "Beasley",
-      email: null,
-      password: null,
+      email: "tanya.beasley@example.com",
+      password: "tanya.beasley",
       phone_number: "789-777-7777"
     },
     {
       user_id: 15,
       first_name: "Ellis",
       last_name: "Joyce",
-      email: null,
-      password: null,
+      email: "ellis.joyce@example.com",
+      password: "ellis.joyce",
       phone_number: "567-555-5555"
     },
     {
       user_id: 16,
       first_name: "Francis",
       last_name: "Webster",
-      email: null,
-      password: null,
+      email: "francis.webster@example.com",
+      password: "francis.webster",
       phone_number: "123-111-1111"
     },
     {
       user_id: 17,
       first_name: "Liberty",
       last_name: "Sanford",
-      email: null,
-      password: null,
+      email: "liberty.sanford@example.com",
+      password: "liberty.sanford",
       phone_number: "222-223-2234"
     },
     {
       user_id: 18,
       first_name: "Jaden",
       last_name: "Kemp",
-      email: null,
-      password: null,
+      email: "jaden.kemp@example.com",
+      password: "jaden.kemp",
       phone_number: "345-333-3333"
     },
   ]);
