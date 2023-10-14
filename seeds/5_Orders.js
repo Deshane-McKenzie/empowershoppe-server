bc57efeae1eff5588f432d0f6ed94669cdb376f9
Deshane McKenzie <122234024+Deshane-McKenzie@users.noreply.github.com>
2023-10-14 16:47:34 -0400

    updated seed file

--- seeds/5_Orders.js
+++ seeds/5_Orders.js
@@ -15,7 +15,6 @@ exports.seed = async function(knex) {
       billing_address_id: 1,
       shipping_address_id: 2,
       total_amount: 20.00,
-      order_status: 0,
     },
     {
       order_id: 2,
@@ -26,7 +25,6 @@ exports.seed = async function(knex) {
       billing_address_id: 3,
       shipping_address_id: 4,
       total_amount: 20.00,
-      order_status: 0,
     }
   ]);
 };
